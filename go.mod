30f325a76e59523464bba2ba1249e38844ae3c7e
Abigail <abigailekene6@gmail.com>
2024-11-20 03:19:26 -0800

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Abigailtony/puppy
 
 go 1.23.0
+
+require github.com/Abigailtony/dog v0.0.0-20241120110426-2554a5667f96 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/Abigailtony/dog"
+
 func Bark() string {
 	return "WOOF!"
 }
 
 func Barks() string {
 	return "WOOF! WOOF! WOOF!"
+}
+
+func Bigbark() string {
+	return dog.WhenAult(Bark())
+}
+
+func BigbarkS() string {
+	return dog.WhenAult(Bark())
 }
